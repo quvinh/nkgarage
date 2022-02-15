881977dfc29b68568f5b659906efc825ce63671c
quvinh <vinhhp2620@gmail.com>
2022-02-15 13:27:59 +0700

    crud permission, roles table

---
+++ app/Http/Controllers/CRUD/Auth/RolesController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\CRUD\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Roles;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $data = Roles::all();
+        return response()->json([
+            'data' => $data
+        ], 201);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $data = Roles::create($request->all());
+
+        return response()->json([
+            'message' => 'Data created successfully',
+            'data' => $data
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $data = Roles::find($id);
+        return response()->json([
+            'status' => 'Show form edit',
+            'message' => 'Show successfully',
+            'data' => $data,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $data = Roles::where('id', $id)->update();
+
+        return response()->json([
+            'message' => 'Data Roles successfully changed',
+            'data' => $data,
+        ], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $data = Roles::find($id);
+        $data->delete();
+
+        return response()->json([
+            'tatus' => 'Delete data Roles',
+            'message' => 'Delete sucessfully',
+        ], 201);
+    }
+}
